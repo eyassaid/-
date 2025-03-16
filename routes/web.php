832d207d4d70cde0e7b1d0250b2fb79755ad6840
eyas <ey6959@gmail.com>
2025-03-16 16:06:24 +0400

    'bring-seed-to-route'

--- routes/web.php
+++ routes/web.php
@@ -19,9 +19,33 @@ Route::resource('teacher.schedule',ScheduleController::class)->except(['show','e
 Route::resource('section.schedule',ScheduleController::class)->except(['show','edit','update']);
 Route::resource('teacher.absent',AbsentController::class)->only(['store','destroy','index']);
 Route::resource('substitute',SubstituteController::class);
-Route::get('/run-seeder', function () {
-    if (TimeTable::all()->count() ==0)
-    Artisan::call('db:seed');
+// Route::get('/run-seeder', function () {
+//     if (TimeTable::all()->count() ==0)
+//     Artisan::call('db:seed');
+//     return to_route('teacher.index');
+// }
+// );
+Route::get('prepare-db',function(){
+    if (TimeTable::all()->count() ==0){
+        $days = ['الاحد','الاثنين','الثلاثاء','الاربعاء','الخميس'];
+        foreach ($days as  $day){
+            for ($i=1; $i<=8; $i++){
+                \App\Models\TimeTable::create(['days'=>$day,'class'=>$i]);
+            }
+        }
+        $teachers = ['إياس بن سعيد بن سعود المعشري', 
+        'أيوب بن يوسف بن حمدون الرحبي','راشد الضاوي','طلال السلماني',
+        'عمران البلوشي','محمد الناعبي', 'اشرف بكري'
+    ];
+    foreach($teachers as $teacher){
+        \App\Models\Teacher::create(['name'=>$teacher]);
+    }
+    $classes = [8,9,10];
+    foreach($classes as $class){
+        for ($i=1; $i<=10; $i++){
+            \App\Models\Section::create(['name'=>"$i/$class"]);
+        }
+    }
+    }
     return to_route('teacher.index');
-}
-);
+});
